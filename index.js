3f26025fd98c589d9486d705aeaeb2db80a48d91
Brandon <gasingdong@gmail.com>
2019-09-01 16:40:13 -0700

    add post user function

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const db = require('./data/db');
 
 const server = express();
 
+server.use(express.json());
+
 server.get('/', (req, res) => {
   res.send('Hello from Express');
 });
@@ -13,10 +15,10 @@ server.get('/api/users', (req, res) => {
       const users = await db.find();
       res.status(200).json(users);
     } catch(err) {
-      res.status(500).send({ error: "The users information could not be retrieved." })
+      res.status(500).json({ error: "The users information could not be retrieved." });
     }
   })();
-})
+});
 
 server.get('/api/users/:id', (req, res) => {
   (async () => {
@@ -24,9 +26,32 @@ server.get('/api/users/:id', (req, res) => {
       const user = await db.findById(req.params.id);
       user
       ? res.status(200).json(user)
-      : res.status(404).send({ message: "The user with the specified ID does not exist." });
+      : res.status(404).json({ message: "The user with the specified ID does not exist." });
     } catch(err) {
-      res.status(500).send({ error: "The user information could not be retrieved." })
+      res.status(500).json({ error: "The user information could not be retrieved." });
+    }
+  })();
+});
+
+server.post('/api/users', (req, res) => {
+  (async () => {
+    try {
+      console.log(req);
+      const { name, bio } = req.body;
+      const timestamp = Date.now();
+      if (name && bio) {
+        const newUser = await db.insert({
+          name,
+          bio,
+          created_at: timestamp,
+          updated_at: timestamp,
+        });
+        res.status(201).json(newUser);
+      } else {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+      }
+    } catch(err) {
+      res.status(500).json({ error: "There was an error while saving the user to the database" });
     }
   })();
 })
