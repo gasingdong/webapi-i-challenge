7efdff65e59a2e24d837669da8a1cbccde9f7caa
Brandon <gasingdong@gmail.com>
2019-09-01 15:59:45 -0700

    add get users response

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express')
+const express = require('express');
+const db = require('./data/db');
 
 const server = express();
 
 server.get('/', (req, res) => {
-    res.send('Hello from Express');
+  res.send('Hello from Express');
 });
 
+server.get('/api/users', (req, res) => {
+  (async () => {
+    try {
+      const users = await db.find();
+      res.status(200).json(users);
+    } catch(err) {
+      res.status(500);
+    }
+  })();
+})
+
 server.listen(5000, () => {
-    console.log('Server running on http://localhost:5000/');
+  console.log('Server running on http://localhost:5000/');
 });
